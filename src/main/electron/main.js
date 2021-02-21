a41d94dc64dae7a5aec9decfaa5cff8a8a993fc7
ucokcode <ucokcode00@gmail.com>
2021-02-21 10:23:35 +0700

    nambahin window

--- src/main/electron/main.js
+++ src/main/electron/main.js
@@ -4,6 +4,7 @@ const args = process.argv.slice(1)
 const serve = args.some(val => val === "--dev")
 const Channel = require("./channel")
 const path = require("path")
+const {data} = require('autoprefixer')
 
 
 let channel = new Channel;
@@ -12,7 +13,7 @@ channel.listen()
 
 
 let mainWindow
-
+let dataBarangWindow
 
 const _root = path.join(__dirname, "..", "..")
 
@@ -26,7 +27,7 @@ const createWindow = () => {
     mainWindow = new BrowserWindow({
         width: 1024,
         height: 600,
-        title: "Aplikasi Kasir",
+        title: "Program Kasir",
         center: true,
         show: false,
         webPreferences: {
@@ -35,9 +36,10 @@ const createWindow = () => {
         }
     });
 
-
+    setMainMenu()
 
     mainWindow.loadURL(`file://${_root}/resources/index.html`);
+
 
     mainWindow.once('ready-to-show', () => {
         mainWindow.show()
@@ -65,3 +67,193 @@ app.on('activate', () => {
     }
 })
 
+
+const createWindowDataBarang = () => {
+    const currentWindow = BrowserWindow.getFocusedWindow()
+
+    let x, y
+
+    if (currentWindow) {
+        const [currentWindowX, currentWindowY] = currentWindow.getPosition();
+        x = currentWindowX;
+        y = currentWindowY;
+    }
+
+
+    dataBarangWindow = new BrowserWindow({
+        width: 400,
+        height: 600,
+        title: "Data Barang",
+        x,
+        y,
+        show: false,
+        webPreferences: {
+            contextIsolation: true,
+            preload: "preload.js"
+        }
+    })
+
+    dataBarangWindow.loadURL(`file://${_root}/resources/databarang.html`);
+
+    dataBarangWindow.setMenu(null)
+
+    dataBarangWindow.once('ready-to-show', () => {
+        dataBarangWindow.show()
+    })
+
+    dataBarangWindow.on('closed', () => {
+        dataBarangWindow = null
+    })
+}
+
+
+const setMainMenu = () => {
+    const template = [
+        {
+            label: 'File',
+            submenu: [
+                {
+                    label: 'Data Barang',
+                    click: () => createWindowDataBarang()
+                },
+            ]
+        },
+        {
+            label: 'View',
+            submenu: [
+                {
+                    label: 'Reload',
+                    accelerator: 'CmdOrCtrl+R',
+                    click(_, focusedWindow) {
+                        if (focusedWindow) focusedWindow.reload()
+                    }
+                },
+                {
+                    label: 'Toggle Developer Tools',
+                    accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+                    click(item, focusedWindow) {
+                        if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+                    }
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'resetzoom'
+                },
+                {
+                    role: 'zoomin'
+                },
+                {
+                    role: 'zoomout'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'togglefullscreen'
+                }
+            ]
+        },
+        {
+            role: 'window',
+            submenu: [
+                {
+                    role: 'minimize'
+                },
+                {
+                    role: 'close'
+                }
+            ]
+        },
+        {
+            role: 'help',
+            submenu: [
+                {
+                    label: 'Learn More',
+                    click() {require('electron').shell.openExternal('http://electron.atom.io')}
+                }
+            ]
+        }
+    ]
+
+    if (process.platform === 'darwin') {
+        const name = app.getName()
+        template.unshift({
+            label: name,
+            submenu: [
+                {
+                    role: 'about'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'services',
+                    submenu: []
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'hide'
+                },
+                {
+                    role: 'hideothers'
+                },
+                {
+                    role: 'unhide'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'quit'
+                }
+            ]
+        })
+        // Edit menu.
+        template[1].submenu.push(
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Speech',
+                submenu: [
+                    {
+                        role: 'startspeaking'
+                    },
+                    {
+                        role: 'stopspeaking'
+                    }
+                ]
+            }
+        )
+        // Window menu.
+        template[3].submenu = [
+            {
+                label: 'Close',
+                accelerator: 'CmdOrCtrl+W',
+                role: 'close'
+            },
+            {
+                label: 'Minimize',
+                accelerator: 'CmdOrCtrl+M',
+                role: 'minimize'
+            },
+            {
+                label: 'Zoom',
+                role: 'zoom'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Bring All to Front',
+                role: 'front'
+            }
+        ]
+    }
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+}
